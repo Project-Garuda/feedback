833f4b8e3edf8fbce6ce5d7c5f33667b6f67eb4f
Venkata Shravan <kandadishravan@gmail.com>
2021-03-17 11:44:12 +0530

    Student user

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, Blueprint, flash, session
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import and_, func
+from flask_bcrypt import Bcrypt
+
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = 'Helloworld'
+bcrypt = Bcrypt(app)
 
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
@@ -28,9 +32,13 @@ app.register_blueprint(mod_student, url_prefix='/student')
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method == "POST":
-        print('DataPassed through post')
-        session['user'] = request.form['login_username']
-        session['secret_key'] = request.form['secretkey']
-        print(request.form)
-        return redirect(url_for('.student.student_dashboard'))
+        user_id = request.form['login_username']
+        if request.form['role'] == 'student':
+            session['user'] = user_id
+            student = Student.query.filter(Student.id == user_id).first()
+            hashed_password = bcrypt.generate_password_hash(request.form['secretkey']).decode('utf-8')
+            if student and bcrypt.check_password_hash(student.password, hashed_password):
+                return redirect(url_for('.student.student_dashboard'))
+            else:
+                return " Login unsuccessful"
     return render_template('index.html')
